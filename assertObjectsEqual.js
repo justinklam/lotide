60abf0072ccc08b5bfac98d0fa6a811da138fa76
Justin Lam <justin.kh.lam@gmail.com>
2021-07-23 01:00:40 +0000

    add assertObjectsEqual.js and update

---
+++ assertObjectsEqual.js
@@ -0,0 +1,17 @@
+const eqObjects = require("./eqObjects");
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; 
+    let lastResult = eqObjects(actual, expected) // eqObjects returns either truthy or falsy values. lastResult is then converted to either
+
+    if (lastResult == false) {
+      return `Comparison Failed: ${inspect(actual)} ❌❌❌ ${inspect(expected)}`;
+    }
+    return `Comparison Passed: ${inspect(actual)} ✅✅✅ ${inspect(expected)}`;
+  };
+
+const objA = {Hands: "Yes"};
+const objB = {Hands: "No"};
+console.log(assertObjectsEqual(objA, objB));
+
+// console.log(`Example label: ${(actual)}`);
