736e8d914278bb4f55e302ecb5c1ec4ec929ddc6
Justin Lam <justin.kh.lam@gmail.com>
2021-08-11 23:16:29 +0000

    rename assertArrayEqual.js with s

---
+++ assertArraysEqual.js
@@ -0,0 +1,18 @@
+const eqArrays = require("./eqArrays");
+
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return `Comparison Failed: ${arr1} ❌❌❌ ${arr2}`;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return `Comparison Failed: ${arr1} ❌❌❌ ${arr2}`;
+    }
+  }
+  return `Comparison Passed: ${arr1} ✅✅✅ ${arr2}`;
+};
+
+module.exports = assertArraysEqual
+
+// eqArrays(assertArraysEqual([1, 2, 3], [1, 2, 3]));
