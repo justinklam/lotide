e67dd36eca282c6bad433793bb15110d76f254de
Justin Lam <justin.kh.lam@gmail.com>
2021-08-11 22:21:07 +0000

    refactor, assertEqual, head

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,28 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+module.exports = assertEqual;
+
 // TEST CODE
 // console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-// console.log(assertEqual(1, 1));
+// console.log(assertEqual("1", 1));
 
-module.exports = assertEqual;
+
+// PREVIOUS CODE
+// const eqArrays = function(actual, expected) {
+
+//   //If array lengths do not match, false.
+//   if (actual.length !== expected.length) {
+//     return false;
+//   }
+
+//   // if array lengths are same, compare each element
+//   for (let i = 0; i < actual.length; i++) {
+//     if (actual[i] !== expected[i]) {
+//       return false;
+//     }
+//   }
+
+//   // if false was not returned, the arrays must be identical
+//   return true;
+// };
