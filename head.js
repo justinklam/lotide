d34654243859910771c1840f74db791e10c737fe
Justin Lam <justin.kh.lam@gmail.com>
2021-08-11 22:34:08 +0000

    head.js refactor

--- head.js
+++ head.js
@@ -1,18 +1,11 @@
 const assertEqual = require("./assertEqual");
 
-// PREVIOUS assertEQUAL function
-// const assertEqual = function(actual, expected) {
-//   if (actual === expected) {
-//     console.log(`Assertion Passed: ${actual} === ${expected}`);
-//   } else console.log(`Assertion Failed: ${actual} === ${expected}`);
-// };
-
 // FUNCTION IMPLEMENTATION
-function head(array) {
+const head = function(array) {
   return array[0];
 };
 
+module.exports = head;
+
 // assertEqual(head([5,6,7]), 5);
 // assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-module.exports = head;
