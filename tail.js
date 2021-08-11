0f72fab25874005cd4ce2d5cd30773cd6de7e896
Justin Lam <justin.kh.lam@gmail.com>
2021-08-11 22:33:03 +0000

    tail.js refacotr

--- tail.js
+++ tail.js
@@ -1,13 +1,12 @@
+const assertEqual = require("./assertEqual");
+
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else console.log(`Assertion Failed: ${actual} === ${expected}`);
+
+const tail = function(array) {
+  return array.slice(1);
 };
 
-function tail(array) {
-  return array.slice(1);
-}
+module.exports = tail;
 
-assertEqual(tail([5,6,7]), 5);
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(tail([5,6,7]), [6,7]);
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
