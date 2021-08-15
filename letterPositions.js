13af1015325d4122ce6a1e4628c1a1ef4a161de7
Justin Lam <justin.kh.lam@gmail.com>
2021-08-15 05:05:54 +0000

    fix code to take in white space into account + proper letter position

--- letterPositions.js
+++ letterPositions.js
@@ -3,16 +3,18 @@ const assertArraysEqual = require("./assertArraysEqual")
 
 const letterPositions = function(sentence) {
   const results = {};
+
   for (let i = 0; i < sentence.length; i++) {
-    if (results.hasOwnProperty(sentence[i])) { // if results has property with sentence[i];
-      results[sentence[i]] += 1; // if yes, increment it
-    } else {
-      results[sentence[i]] = 1; // if no, add 1 to it
+    if (sentence[i] !== " ") { // if sentence[i] does not contain whitespace
+      if (results[sentence[i]]) { // and if results contains sentence[i]
+        results[sentence[i]].push(i); // if yes, push index position into object under index character
+      } else {
+        results[sentence[i]] = [i]; // if no, add [i] into object
+      }
     }
   }
   return results;
+  
 };
 
 module.exports = letterPositions
-
-console.log(letterPositions("hello"))
